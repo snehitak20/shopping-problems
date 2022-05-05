51c04680dc7a830c8d9a799ff1e7010e82ce2df1
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-04 23:51:31 -0700

    finish server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,6 +42,7 @@ router.get('/:id', (req, res) => {
   .then(dbCategoryData => {
     if (!dbCategoryData) {
       res.status(404).json({ message: 'No category found with this id' });
+      return;
     }
     res.json(dbCategoryData);
   })
@@ -75,6 +76,7 @@ router.put('/:id', (req, res) => {
   .then(dbCategoryData => {
     if (!dbCategoryData[0]) {
       res.status(404).json({ message: 'No category found with this id' });
+      return;
     }
     res.json(dbCategoryData);
   })
@@ -94,6 +96,7 @@ router.delete('/:id', (req, res) => {
   .then(dbCategoryData => {
     if (!dbCategoryData) {
       res.status(404).json({ message: 'No category found with this id' });
+      return;
     }
     res.json(dbCategoryData);
   })
